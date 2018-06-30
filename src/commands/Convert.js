aa72339bd763ad01f97f5653e740e3c17499dbb0
Romain BERTOLUCCI <romain.bertolucci@gmail.com>
2018-06-30 13:26:27 +0200

    Modifying convert command, it's now more understandable

--- src/commands/Convert.js
+++ src/commands/Convert.js
@@ -1,9 +1,9 @@
 export default (state, payload) => {
-    let moneyBuy = payload[0].toLowerCase();
-    let moneySell = (moneyBuy === 'eur') ? 'btc' : 'eur';
+    let moneyFrom = payload[0].toLowerCase();
+    let moneyTo = (moneyFrom === 'eur') ? 'btc' : 'eur';
     let value = payload[1];
 
-    if (!['eur', 'btc'].includes(moneyBuy)) {
+    if (!['eur', 'btc'].includes(moneyFrom)) {
         state.logs.push({
             output: `The money must be 'eur' or 'btc'`,
             type: 'error'
@@ -19,23 +19,24 @@ export default (state, payload) => {
         return false;
     }
 
-    let base = parseFloat((moneyBuy === 'eur') ? state.balance.btc : state.balance.eur);
-    let rate = ((moneyBuy === 'eur') ? state.balance.rate : (1 / state.balance.rate)).toFixed(5);
-    let change = (value * rate).toFixed(2);
+    let base = parseFloat((moneyFrom === 'eur') ? state.balance.eur : state.balance.btc);
 
     if (value > base) {
         state.logs.push({
-            output: `You don't have enough ${(moneyBuy === 'eur') ? 'bitcoins' : 'euros'}`,
+            output: `You don't have enough ${(moneyFrom === 'eur') ? 'euros' : 'bitcoins'}`,
             type: 'error'
         });
         return false;
     }
 
-    state.balance[moneyBuy] += parseFloat(change);
-    state.balance[moneySell] -= parseFloat(value);
+    let rate = ((moneyFrom === 'eur') ? state.balance.rate : (1 / state.balance.rate)).toFixed(5);
+    let change = (value * rate).toFixed(2);
+
+    state.balance[moneyFrom] -= parseFloat(value);
+    state.balance[moneyTo] += parseFloat(change);
 
     state.logs.push({
-        output: `You have converted ${value} ${(moneyBuy === 'eur') ? 'bitcoins' : 'euros'} for ${change} ${(moneyBuy === 'eur') ? 'euros' : 'bitcoins'} (Change rate: ${rate})`,
+        output: `You have converted ${value} ${(moneyFrom === 'eur') ? 'euros' : 'bitcoins'} for ${change} ${(moneyFrom === 'eur') ? 'bitcoins' : 'euros'} (Change rate: ${rate})`,
         type: 'success'
     });
 }
